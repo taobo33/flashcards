3713d77e4d22020cb3cf3f51103c2664f606215e
Bozhong Tao <3196115@qq.com>
2018-06-01 00:08:54 +0800

    Finished stage 4 tasks

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 
+app.set('view engine', 'pug')
+
+const mainRoutes = require('./routes/index');
+const cardRoutes = require('./routes/cards')
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
 const friendNames = [
     {First: 'Su', Last: 'Wu'},
     {First: 'Wodi', Last: 'Chen'},
@@ -16,47 +24,20 @@ const friendNames = [
     {First: 'Shiyang', Last: 'Yao'}
 ];
 
-app.set('view engine', 'pug')
-
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name: name });
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?"});
-    // Another way to do it
-    // res.locals.prompt = "Who is buried in Grant's tomb?";
-    // res.render('card');
-});
-
-// Add hello route to prompt username input or redirect
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('./');
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-// Add Goodbye route to clear out cookie
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
 app.get('/challenge', (req, res) => {
     res.render('sandbox', {friendNames});
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
